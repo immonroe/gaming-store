3c4b955c0f23278ec5f3eeb48b5c2235829da78b
immonroe <immanuelmonroe@gmail.com>
2023-08-28 17:57:16 -0400

    Reconfigured nav and added bootstrap/react icons

--- src/components/layout/MainNavigation.jsx
+++ src/components/layout/MainNavigation.jsx
@@ -1,43 +1,42 @@
-// MainNavigation.jsx
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import { FaShoppingCart } from 'react-icons/fa';
+import { AiOutlineSearch, AiFillGithub } from 'react-icons/ai'
+import { Link } from 'react-router-dom';
 import styles from './MainNavigation.module.css';
 
 const MainNavigation = () => {
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-
-  const toggleMobileMenu = () => {
-    setIsMobileMenuOpen((prevState) => !prevState);
-  };
-
   return (
+
+    
+
     <nav className={styles.nav}>
-      <div className={styles.logo}>Your Game Store Logo</div>
-      <div className={`${styles.navItems} ${isMobileMenuOpen ? styles.open : ''}`}>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/store">Store</Link>
-          </li>
-        </ul>
+
+    {/* <ul>
+        <li>
+        <Link to="/">Home</Link>
+        </li>
+        <li>
+        <Link to="/about">About</Link>
+        </li>
+        <li>
+        <Link to="/store">Store</Link>
+        </li>
+    </ul> */}
+
+      <div className={styles.navLeft}>
+        <div className={styles.logo}>Game Store</div>
         <div className={styles.searchBar}>
-          <input type="text" placeholder="Search for games" />
-        </div>
-        <div className={styles.cart}>
-          <FaShoppingCart className={styles.cartIcon} />
-          <span className={styles.cartCount}>0</span>
+          <input type="text" placeholder="Search for games..." />
+          <button className={styles.searchIcon}>
+          <AiOutlineSearch />
+          </button>
         </div>
       </div>
-      <div className={styles.mobileToggle} onClick={toggleMobileMenu}>
-        <div className={`${styles.bar} ${isMobileMenuOpen ? styles.open : ''}`} />
-        <div className={`${styles.bar} ${isMobileMenuOpen ? styles.open : ''}`} />
-        <div className={`${styles.bar} ${isMobileMenuOpen ? styles.open : ''}`} />
+      <div className={styles.navRight}>
+        <AiFillGithub />
+        <a href="https://github.com/yourusername" className={styles.githubLink}>
+          <FaShoppingCart className={styles.cartIcon} />
+        </a>
       </div>
     </nav>
   );
