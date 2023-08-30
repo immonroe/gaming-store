b9f849eb517cf4b7c0ee2cd425e3c1496e80ef5b
immonroe <immanuelmonroe@gmail.com>
2023-08-30 09:21:22 -0400

    Fixed responsiveness of navbar/added icons

--- src/components/layout/MainNavigation.jsx
+++ src/components/layout/MainNavigation.jsx
@@ -1,17 +1,39 @@
 import React from 'react';
 import { FaShoppingCart } from 'react-icons/fa';
-import { AiOutlineSearch, AiFillGithub } from 'react-icons/ai'
+import { AiFillGithub } from 'react-icons/ai';
+import { IoGameControllerOutline } from 'react-icons/io5';
+import { ImSearch } from "react-icons/im";
 import { Link } from 'react-router-dom';
 import styles from './MainNavigation.module.css';
 
 const MainNavigation = () => {
-  return (
+    return (
+        <nav className={styles.nav}>
+            <div className={styles.navLeft}>
+                <div className={styles.logo}>
+                    <IoGameControllerOutline />
+                    <span>Game Store</span>
+                </div>
+                <form className={styles.searchBar}>
+                    <input type="text" placeholder="Search for games..." />
+                    <button className={styles.searchIcon}>
+                        <ImSearch size={'1.125rem'} />
+                    </button>
+                </form>
+            </div>
+            <div className={styles.navRight}>
+                <a href="https://github.com/yourusername" className={styles.githubLink}>
+                    <AiFillGithub size={'1.5rem'} />
+                </a>
+                <FaShoppingCart size={'1.5rem'} className={styles.cartIcon} />
+            </div>
+        </nav>
+    );
+};
 
-    
+export default MainNavigation;
 
-    <nav className={styles.nav}>
-
-    {/* <ul>
+{/* <ul>
         <li>
         <Link to="/">Home</Link>
         </li>
@@ -22,24 +44,3 @@ const MainNavigation = () => {
         <Link to="/store">Store</Link>
         </li>
     </ul> */}
-
-      <div className={styles.navLeft}>
-        <div className={styles.logo}>Game Store</div>
-        <div className={styles.searchBar}>
-          <input type="text" placeholder="Search for games..." />
-          <button className={styles.searchIcon}>
-          <AiOutlineSearch />
-          </button>
-        </div>
-      </div>
-      <div className={styles.navRight}>
-        <AiFillGithub />
-        <a href="https://github.com/yourusername" className={styles.githubLink}>
-          <FaShoppingCart className={styles.cartIcon} />
-        </a>
-      </div>
-    </nav>
-  );
-};
-
-export default MainNavigation;
