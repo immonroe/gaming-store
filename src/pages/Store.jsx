1f850abf0bf24dd7150aef09d949ddd044e0f7a4
immonroe <immanuelmonroe@gmail.com>
2023-09-30 11:06:38 -0400

    Added routing to dynamic links on store page

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,4 +1,5 @@
 import data from '../db/data.json'
+import { Link } from 'react-router-dom';
 
 function Store() {
   return (
@@ -6,7 +7,7 @@ function Store() {
       <h1>Store Page</h1>
       {/* render dynamic list into JSX elements */}
       {data.games.map((games) => {
-        return <li key={games.id}>{games.title}</li>
+        return <li key={games.id}><Link to={`/store/${games.id}`}>{games.title}</Link></li>
       })}
     </div>
   );
