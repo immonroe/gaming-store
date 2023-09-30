453260e10cee1f76bd92df5a616ea9e85da170bd
immonroe <immanuelmonroe@gmail.com>
2023-09-30 14:06:55 -0400

    Added spacing between cards on store page

--- src/components/store/Cards.jsx
+++ src/components/store/Cards.jsx
@@ -5,40 +5,59 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 // Database
-import data from '../../db/data.json'
+import data from '../../db/data.json';
 
 // Styling
-import classes from '../store/Cards.module.css'
+import classes from '../store/Cards.module.css';
 
 function Cards() {
+  const maxCardHeight = '350px'; // Adjust this value to your desired fixed height
+
   return (
-    <div>
-      {data.games.map((game) => (
-        <Card key={game.id} sx={{ maxWidth: 345 }}>
-          <CardMedia sx={{ height: 140 }} image={game.imageUrl} title={game.title} />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              {game.title}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {game.description}
-            </Typography>
-          </CardContent>
-          <CardActions className={classes.cartContainer}>
-            <Button size="small">Add to Cart</Button>
-            <Typography variant="body2" color="text.primary">
-              Price: ${game.price.toFixed(2)}
-            </Typography>
-          </CardActions>
-        </Card>
-      ))}
-    </div>
+    <Container>
+      <Row xs={1} md={2} lg={3} gap={1}> {/* Use xs, md, lg to control the number of columns */}
+        {data.games.map((game) => (
+          <Col key={game.id} className="mb-4"> {/* Add margin-bottom for spacing */}
+            <Card
+              sx={{
+                maxWidth: 345,
+                height: maxCardHeight, // Set a fixed height for the card
+                display: 'flex',
+                flexDirection: 'column',
+              }}
+            >
+              <CardMedia sx={{ height: 160 }} image={game.imageUrl} title={game.title} />
+              <CardContent
+                style={{
+                  flex: '1',
+                  overflow: 'hidden',
+                  maxHeight: `calc(${maxCardHeight} - 160px)`, // Adjust for image height
+                }}
+              >
+                <Typography gutterBottom variant="h5" component="div">
+                  {game.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {game.description}
+                </Typography>
+              </CardContent>
+              <CardActions className={classes.cartContainer}>
+                <Button size="small">Add to Cart</Button>
+                <Typography variant="body2" color="text.primary">
+                  Price: ${game.price.toFixed(2)}
+                </Typography>
+              </CardActions>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 }
 
 export default Cards;
-
-
