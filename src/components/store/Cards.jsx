d799ab430860fef727230a0a974dacf526056bf3
immonroe <immanuelmonroe@gmail.com>
2023-10-03 09:08:43 -0400

    Added dynamic links to cards on store page

--- src/components/store/Cards.jsx
+++ src/components/store/Cards.jsx
@@ -8,6 +8,7 @@ import Typography from '@mui/material/Typography';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { Link } from 'react-router-dom'; // Import Link from React Router
 
 // Database
 import data from '../../db/data.json';
@@ -20,39 +21,41 @@ function Cards() {
 
   return (
     <Container>
-      <Row xs={1} md={2} lg={3} gap={1}> {/* Use xs, md, lg to control the number of columns */}
+      <Row xs={1} md={2} lg={3} gap={1}>
         {data.games.map((game) => (
-          <Col key={game.id} className="mb-4"> {/* Add margin-bottom for spacing */}
-            <Card
-              sx={{
-                maxWidth: 345,
-                height: maxCardHeight, // Set a fixed height for the card
-                display: 'flex',
-                flexDirection: 'column',
-              }}
-            >
-              <CardMedia sx={{ height: 180 }} image={game.imageUrl} title={game.title} />
-              <CardContent
-                style={{
-                  flex: '1',
-                  overflow: 'hidden',
-                  maxHeight: `calc(${maxCardHeight} - 180px)`, // Adjust for image height
+          <Col key={game.id} className="mb-4">
+            <Link to={`/store/${game.id}`} className="text-decoration-none"> {/* Wrap Card with Link */}
+              <Card
+                sx={{
+                  maxWidth: 345,
+                  height: maxCardHeight,
+                  display: 'flex',
+                  flexDirection: 'column',
                 }}
               >
-                <Typography gutterBottom variant="h5" component="div">
-                  {game.title}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {game.description}
-                </Typography>
-              </CardContent>
-              <CardActions className={classes.cartContainer}>
-                <Button size="small">Add to Cart</Button>
-                <Typography variant="body2" color="text.primary">
-                  Price: ${game.price.toFixed(2)}
-                </Typography>
-              </CardActions>
-            </Card>
+                <CardMedia sx={{ height: 180 }} image={game.imageUrl} title={game.title} />
+                <CardContent
+                  style={{
+                    flex: '1',
+                    overflow: 'hidden',
+                    maxHeight: `calc(${maxCardHeight} - 180px)`,
+                  }}
+                >
+                  <Typography gutterBottom variant="h5" component="div">
+                    {game.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {game.description}
+                  </Typography>
+                </CardContent>
+                <CardActions className={classes.cartContainer}>
+                  <Button size="small">Add to Cart</Button>
+                  <Typography variant="body2" color="text.primary">
+                    Price: ${game.price.toFixed(2)}
+                  </Typography>
+                </CardActions>
+              </Card>
+            </Link>
           </Col>
         ))}
       </Row>
