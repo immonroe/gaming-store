a85bf29d38933c6a326aa98a76103e63c071409b
immonroe <immanuelmonroe@gmail.com>
2023-10-11 16:42:40 -0400

    Updated firebase functionality

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -1,39 +1,70 @@
-import React, { createContext, useState } from "react"
-import data from '../db/data.json'
+import React, { createContext, useState, useContext, useEffect } from "react";
+import { getFirestore, collection, doc, setDoc } from 'firebase/firestore';
+import data from '../db/data.json';
+import { firestore } from "../firebase/firebase";
 
 export const ShopContext = createContext(null);
 
 const getDefaultCart = () => {
     let cart = {};
-    for (let i=1; i < data.games.length + 1; i++) {
+    for (let i = 1; i <= data.games.length; i++) {
         cart[i] = 0;
     }
     return cart;
-}
+};
 
 export const ShopContextProvider = (props) => {
-    const [cartItems, setCartItems] = useState(getDefaultCart());
-  
+    // Load cart data from localStorage (if available) or use the default cart
+    const initialCart = JSON.parse(localStorage.getItem('cart')) || getDefaultCart();
+    const [cartItems, setCartItems] = useState(initialCart);
+
     const addToCart = (itemId) => {
-      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+        const updatedCart = { ...cartItems, [itemId]: cartItems[itemId] + 1 };
+        setCartItems(updatedCart);
+        // Save the updated cart to localStorage
+        localStorage.setItem('cart', JSON.stringify(updatedCart));
     };
-  
+
     const removeFromCart = (itemId) => {
-        setCartItems((prev) => ({ ...prev, [itemId]: 0 })); // Set quantity to 0 to remove all items
+        const updatedCart = { ...cartItems, [itemId]: 0 };
+        setCartItems(updatedCart);
+        // Save the updated cart to localStorage
+        localStorage.setItem('cart', JSON.stringify(updatedCart));
     };
-  
+
     const updateQuantity = (itemId, newQuantity) => {
-      if (newQuantity >= 0) {
-        setCartItems((prev) => ({ ...prev, [itemId]: parseInt(newQuantity) }));
-      }
+        if (newQuantity >= 0) {
+            const updatedCart = { ...cartItems, [itemId]: newQuantity };
+            setCartItems(updatedCart);
+            // Save the updated cart to localStorage
+            localStorage.setItem('cart', JSON.stringify(updatedCart));
+        }
     };
-  
+
+    // This function updates the cart data in Firebase
+    const updateCartInFirebase = (userId, cartData) => {
+        const userCartRef = doc(collection(firestore, 'carts'), userId);
+
+        setDoc(userCartRef, { cartData }, { merge: true })
+            .then(() => {
+                console.log('Cart data successfully updated in Firestore');
+            })
+            .catch((error) => {
+                console.error('Error updating cart data: ', error);
+            });
+    };
+
+    useEffect(() => {
+        // Update the cart in Firebase when cartItems change
+        // Replace 'your-user-id' with the actual user's ID
+        updateCartInFirebase('your-user-id', cartItems);
+    }, [cartItems]);
+
     const contextValue = { cartItems, addToCart, removeFromCart, updateQuantity };
-    console.log(cartItems)
+
     return (
-      <ShopContext.Provider value={contextValue}>
-        {props.children}
-      </ShopContext.Provider>
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
     );
-  };
-  
+};
--- src/firebase/firebase.jsx
+++ src/firebase/firebase.jsx
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "@firebase/firestore"
-
+import { getFirestore } from "@firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,7 +10,7 @@ import { getFirestore } from "@firebase/firestore"
 const firebaseConfig = {
   apiKey: "AIzaSyDizFu1nnmvD5CDTgWs9zyeIImbbljRF_k",
   authDomain: "gaming-store-503cc.firebaseapp.com",
-  databaseURL: "https://gaming-store-503cc-default-rtdb.firebaseio.com/",
+  databaseURL: "https://gaming-store-503cc-default-rtdb.firebaseio.com",
   projectId: "gaming-store-503cc",
   storageBucket: "gaming-store-503cc.appspot.com",
   messagingSenderId: "834624323705",
