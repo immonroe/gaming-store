76b04ccbb39d6a2e39238744ee4117d456d20d98
immonroe <immanuelmonroe@gmail.com>
2023-10-11 09:57:27 -0400

    commented out cartItem import

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import data from '../db/data.json'
 import { Link } from 'react-router-dom';
 import { ShopContext } from '../context/shop-context';
-import { CartItem } from '../components/cart-item'
+// import { CartItem } from '../components/cart-item'
 
 
 
@@ -17,11 +17,11 @@ function Cart() {
         <h1 className="d-flex justify-content-center mb-4">Cart</h1>
         </div>
         <div className="cartItems">
-      {data.games.map((games) => {
+      {/* {data.games.map((games) => {
         if (cartItems[games.id] !== 0) {
           return <cartItem key={data.games} />;
         }
-      })}
+      })} */}
         </div>
       </div>
     );
