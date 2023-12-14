4d58197124389eda4d092288c7c40e4f536d87b8
immonroe <immanuelmonroe@gmail.com>
2023-12-14 15:07:32 -0500

    Removed wishlist template code

--- src/pages/wishlist/Wishlist.jsx
+++ src/pages/wishlist/Wishlist.jsx
@@ -1,37 +1,10 @@
-// function Wishlist() {
-//     return (
-//       <div>
-//         <h1 className="d-flex justify-content-center mb-4">Wishlist</h1>
-//         <div className='d-flex justify-content-center'>Coming soon...</div>
-//       </div>
-//     );
-//   }
-
-import React from 'react';
-
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-function Wishlist(){
-  const notify = () => toast("Game added to cart!");
-
-  return (
-    <div>
-      <button onClick={notify}>Add to cart</button>
-      <ToastContainer
-        position="bottom-right"
-        autoClose={2500}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
-    </div>
-  );
-}
+function Wishlist() {
+    return (
+      <div>
+        <h1 className="d-flex justify-content-center mb-4">Wishlist</h1>
+        <div className='d-flex justify-content-center'>Coming soon...</div>
+      </div>
+    );
+  }
   
-  export default Wishlist;
+export default Wishlist;
